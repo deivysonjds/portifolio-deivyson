dab21662db56e8ccd10436ccd3c3cafeeb4af9d0
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-06-01 18:30:05 -0300

    refactor: mobile responsiveness

--- deivyson-portfolio/src/components/Hero.jsx
+++ deivyson-portfolio/src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
 
   return (
     <section id="home" className={`min-h-screen flex items-center pt-20 ${theme === 'dark' ? 'bg-gray-900' : 'bg-white'} transition-colors duration-300`}>
-      <div className="container mx-auto px-6 flex flex-col md:flex-row items-center">
+      <div className="container mx-auto px-6 flex flex-col-reverse md:flex-row items-center">
         <div className="md:w-1/2">
           <h1 className={`text-4xl md:text-6xl font-bold mb-4 ${theme === 'dark' ? 'text-white' : 'text-gray-800'}`}>
             Olá, me chamo <span className="text-purple-500">Deivyson!</span>
@@ -21,9 +21,8 @@ export default function Hero() {
           </p>
           <SocialIcons className="mt-8" />
         </div>
-        <div className="md:w-1/3 mt-10 md:mt-0">
-          <div className="relative mx-auto mt-20">
-
+        <div className="w-64 md:w-1/3 md:mt-0 flex justify-center items-start">
+          <div className="relative mx-auto ">
             <div className="relative z-10 w-full h-full rounded-full overflow-hidden border-4 border-purple-500 shadow-lg hover:scale-105 transition transform">
               <img
                 src="/imgs/perfil.JPG"
