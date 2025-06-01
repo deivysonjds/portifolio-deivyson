2857c47086d250aa7bd5dd8f34964d5503f7ffed
Deivyson Silva <deivyson.jd.silva@gmail.com>
2025-05-31 22:49:46 -0300

    refactor: header

--- deivyson-portfolio/src/components/Header.jsx
+++ deivyson-portfolio/src/components/Header.jsx
@@ -10,17 +10,14 @@ export default function Header() {
 
   return (
     <header className={`fixed w-full z-50 ${theme === 'dark' ? 'bg-gray-900' : 'bg-white'} shadow-md`}>
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold">
-          <span className={theme === 'dark' ? 'text-purple-400' : 'text-purple-600'}>Portfolio</span>
-        </Link>
+      <div className="container mx-auto px-6 py-4 flex justify-end items-center">
         
-        <nav className="hidden md:flex space-x-8">
-          <Link href="#home" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Home</Link>
-          <Link href="#about" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>About</Link>
-          <Link href="#skills" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Skills</Link>
-          <Link href="#projects" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Projects</Link>
-          <Link href="#contact" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Contact</Link>
+        <nav className="hidden md:flex space-x-8 mr-14">
+          <Link href="#home" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Início</Link>
+          <Link href="#about" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Sobre mim</Link>
+          <Link href="#skills" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Habilidades</Link>
+          <Link href="#projects" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Projetos</Link>
+          <Link href="#contact" className={`${"hover:text-purple-500 transition"} ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Contato</Link>
         </nav>
         
         <ThemeToggle />
